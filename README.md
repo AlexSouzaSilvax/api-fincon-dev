23aac99616ee57a4463166e13feecd212d4a630a
Alex Souza da Silva <alexsouzasilvax@gmail.com>
2022-05-26 02:36:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# api-fincon-dev
+API desenvolvida em NODEJS para estudo.
+
+Fiz o deploy dela no heroku, segue link abaixo:
+
+http://api-fincon-dev.herokuapp.com/
---
+++ src/routes.js
@@ -0,0 +1,101 @@
+const express = require("express");
+
+const LancamentoController = require("./controllers/LancamentoController");
+
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+  return res.json({
+    DescricaO: "API FINCON",
+    Github: "https://github.com/alexsouzasilvax/api-fincon-dev",
+    Twitter: "https://twitter.com/alexsouzasilvax",
+    LinkedIn: "https://www.linkedin.com/in/alexsouzasilvax/",
+    rotas: [
+      {
+        //lancamento
+        Lancamento: [
+          {
+            lista: [
+              {
+                method: "GET",
+                route: "/fincon/lancamentos",
+              },
+            ],
+            buscar: [
+              {
+                method: "POST",
+                route: "/fincon/lancamento/buscar",
+                body: [
+                  {
+                    _id: "_id",
+                  },
+                ],
+              },
+            ],
+            criar: [
+              {
+                method: "POST",
+                route: "/fincon/lancamento/criar",
+                body: [
+                  {
+                    tipo: "String",
+                    descricao: "String",
+                    valor: "String",
+                    mensal: "String",
+                    pagamento: "String",
+                    parcelas: "String",
+                    dataPagamento: "String",
+                    mesReferencia: "String",
+                    dataVencimento: "String",
+                    observacao: "String",
+                    usuario: "_id",
+                  },
+                ],
+              },
+            ],
+            atualizar: [
+              {
+                method: "POST",
+                route: "/fincon/lancamento/atualizar",
+                body: [
+                  {
+                    _id: "_id",
+                    tipo: "String",
+                    descricao: "String",
+                    valor: "String",
+                    mensal: "String",
+                    pagamento: "String",
+                    parcelas: "String",
+                    dataPagamento: "String",
+                    mesReferencia: "String",
+                    dataVencimento: "String",
+                    observacao: "String",
+                    usuario: "_id",
+                  },
+                ],
+              },
+            ],
+            apagar: [
+              {
+                method: "POST",
+                route: "/fincon/lancamento/apagar",
+                body: [
+                  {
+                    _id: "_id",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+routes.post("/fincon/lancamentos", LancamentoController.index);
+routes.post("/fincon/lancamento/criar", LancamentoController.store);
+routes.post("/fincon/lancamento/atualizar", LancamentoController.update);
+routes.post("/fincon/lancamento/apagar", LancamentoController.destroy);
+
+module.exports = routes;
